999649a34756e5d9c6d8e95851e5ad422bc87e1e
cristiam <cristiamherrera@gmail.com>
2021-04-20 10:20:03 -0400

    Se adicionaron los campos a la tabla usuarios

--- database/migrations/2021_03_02_192923_add_campos_users_table.php
+++ database/migrations/2021_03_02_192923_add_campos_users_table.php
@@ -14,13 +14,15 @@ class AddCamposUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('sector_id')->nullable()->after('id');
-            $table->foreign('sector_id')->references('id')->on('sectores');
+            $table->string('ap_paterno', 15)->nullable()->after('password');
+            $table->string('ap_materno', 15)->nullable()->after('password');
+            $table->string('carnet', 20)->nullable()->after('password');
+            $table->string('genero', 10)->nullable()->after('password');
+            $table->string('estado_civil', 15)->nullable()->after('password');
             $table->string('perfil', 30)->nullable()->after('password');
             $table->string('celulares', 30)->nullable()->after('password');
+            $table->string('email', 30)->nullable()->after('password');
             $table->string('direccion', 200)->nullable()->after('password');
-            $table->string('latitud', 30)->nullable()->after('password');
-            $table->string('longitud', 30)->nullable()->after('password');
             $table->string('estado', 30)->nullable()->after('password');
             $table->date('fecha_nacimiento')->nullable()->after('password');
             $table->datetime('deleted_at')->nullable()->after('remember_token');
@@ -36,13 +38,15 @@ class AddCamposUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropForeign(['sector_id']);
             $table->dropColumn('sector_id');
+            $table->dropColumn('ap_paterno');
+            $table->dropColumn('ap_materno');
+            $table->dropColumn('carnet');
+            $table->dropColumn('genero');
+            $table->dropColumn('estado_civil');
             $table->dropColumn('perfil');
             $table->dropColumn('celulares');
             $table->dropColumn('direccion');
-            $table->dropColumn('latitud');
-            $table->dropColumn('longitud');
             $table->dropColumn('estado');
             $table->dropColumn('fecha_nacimiento');
             $table->dropColumn('deleted_at');
